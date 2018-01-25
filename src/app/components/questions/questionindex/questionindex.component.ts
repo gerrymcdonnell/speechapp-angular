6bb6a4b62b495cb60007cd893593140e4e00ffb3
Gerry Mc Donnell <irishbloke@gmail.com>
2018-01-25 15:47:53 +0000

    question index working

--- src/app/components/questions/questionindex/questionindex.component.ts
+++ src/app/components/questions/questionindex/questionindex.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Question} from '../../../models/question';
 
+import {QuestionsService} from '../../../services/questions.service';
 @Component({
   selector: 'app-questionindex',
   templateUrl: './questionindex.component.html',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionindexComponent implements OnInit {
 
-  constructor() { }
+  //properties
+  questions:Question[];
+
+
+  //default constrctor
+  //Service must be added to component constructor
+  constructor(public dataService:QuestionsService){
+      
+      //ex1
+      //console.log(words);
+      
+      //use speechapp data service
+      this.dataService.getQuestions().subscribe(questions=>{
+          this.questions=questions;
+          console.log(this.questions);
+      })
+
+  }//end constructor
 
   ngOnInit() {
   }
